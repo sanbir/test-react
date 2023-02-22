dddb6aff6e20a40497132c8f192a3b6838e8d70d
Aleksei Zhuikov <design.azv@gmail.com>
2023-02-22 21:27:04 +0300

    added name Aleksey

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 //import {FunctionComponent} from "./FunctionComponent";
 //import {List} from "./List";
 //import {ComponentWithState} from "./ComponentWithState";
-import {ContextUsage} from "./ContextUsage";
+import { ContextUsage } from "./ContextUsage";
 
 function App() {
   return (
-    <div>
-        <ContextUsage/>
-    </div>
+    <>
+      <div>
+        <ContextUsage />
+      </div>
+      <div>My name is Aleksei</div>
+    </>
+
   );
 }
 
